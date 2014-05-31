69b217dd9f1a5342bfcb53939338e9c03e17bac3
ashesean <seanashe@Seans-MacBook-Air.local>
2014-05-31 15:37:09 -0700

    BinaryNode
    Same as Clare’s

---
+++ BinaryNode.h
@@ -0,0 +1,34 @@
+// Node for a binary tree
+// Created by Frank M. Carrano and Tim Henry.
+// Modified by CNguyen
+ 
+#ifndef _BINARY_NODE
+#define _BINARY_NODE
+
+template<class ItemType>
+class BinaryNode
+{   
+private:
+	ItemType              item;         // Data portion
+	BinaryNode<ItemType>* leftPtr;		// Pointer to left child
+	BinaryNode<ItemType>* rightPtr;		// Pointer to right child
+
+public:
+	// constructors
+	BinaryNode(const ItemType & anItem)			   {item = anItem; leftPtr = 0; rightPtr = 0;}
+	BinaryNode(const ItemType & anItem, 
+		       BinaryNode<ItemType>* left, 
+			   BinaryNode<ItemType>* right)		   {item = anItem; leftPtr = left; rightPtr = right;}
+	// accessors
+	void setItem(const ItemType & anItem)		   {item = anItem;}
+	void setLeftPtr(BinaryNode<ItemType>* left)	   {leftPtr = left;}
+	void setRightPtr(BinaryNode<ItemType>* right)  {rightPtr = right;}
+	// mutators
+	ItemType getItem() const					   {return item;}
+	BinaryNode<ItemType>* getLeftPtr() const	   {return leftPtr;}
+	BinaryNode<ItemType>* getRightPtr() const	   {return rightPtr;}
+
+	bool isLeaf() const							   {return (leftPtr == 0 && rightPtr == 0);}
+}; 
+
+#endif 
