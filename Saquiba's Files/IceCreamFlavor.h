b241d44961e6a5447ae6a6550f5b043c200d08e2
dvn7035 <dvn7035@gmail.com>
2014-06-02 22:31:26 -0700

    Corrected Logic Errors

--- Saquiba's Files/IceCreamFlavor.h
+++ Saquiba's Files/IceCreamFlavor.h
@@ -1,8 +1,6 @@
 #ifndef ICECREAMFLAVOR_H_INCLUDED
 #define ICECREAMFLAVOR_H_INCLUDED
 
-
-
 class IceCreamFlavor
 {
 private:
@@ -12,7 +10,7 @@ private:
     bool nuts;
 public:
 	//default constructor
-	IceCreamFlavor(){name = "";price = 0; calories =0, nuts =false;}
+	IceCreamFlavor() {name = "";price = 0; calories = 0, nuts = false;}
     //Constructor
     IceCreamFlavor(string input_name, double input_price, int input_calories, bool input_nuts)
     {
@@ -21,61 +19,29 @@ public:
         calories = input_calories;
         nuts = input_nuts;
     }
-	void setName(string str) {  name = str;  }
-	void setPrice(double num)  {   price = num;   }
-	void setcal(int str)  {  calories = str;  }
-	void setnuts(bool nut)	{nuts = nut;}
-	
+	void setName(string str) {name = str;}
+	void setPrice(double num) {price = num;}
+	void setcal(int cal) {calories = cal;}
+	void setnuts(bool nut) {nuts = nut;}
 
-// Accessors
+    // Accessors (David: I think some of these are unnesseary)
 
-	string getName() const
-	{
-		return name;
-	}
-
-	bool getNuts() const
-	{
-		return nuts;
-	}
-
-	int getCalories() const
-	{
-		return calories;
-	}
-
-	double getPrice() const
-	{
-		return price;
-	}
+	string getName() const {return name;}
+	bool getNuts() const {return nuts;} //getNuts lol
+	int getCalories() const {return calories;}
+	double getPrice() const {return price;}
 
 	// Operator Overloads
 
-	friend ostream& operator<<(ostream &os, const IceCreamFlavor &obj)
+	friend ostream& operator<< (ostream &os, const IceCreamFlavor &obj)
 	{
 		return os << obj.name;
 	}
-
-	bool operator>(const IceCreamFlavor &right)
-	{
-		return (name > right.name);	// ? true : false; 
-	}
-	bool operator<(const IceCreamFlavor &right)
-	{
-		return !(name > right.name);
-	}
-
-	bool operator==(const IceCreamFlavor &right)
-	{
-		return (name == right);// ? true : false;
-	}
-
-	bool operator!=(const IceCreamFlavor &right)
-	{
-		return !(name == right.name);
-	}
-
-	const IceCreamFlavor operator=(const IceCreamFlavor &right)
+	bool operator> (const IceCreamFlavor &right) {return (name > right.name);}
+	bool operator< (const IceCreamFlavor &right) {return (name < right.name);}
+	bool operator== (const IceCreamFlavor &right) {return (name == right.name);} // ? true : false;
+	bool operator!= (const IceCreamFlavor &right) {return !operator(right);}
+    IceCreamFlavor& operator= (const IceCreamFlavor &right)
 	{
 		name = right.name;
 		nuts = right.nuts;
@@ -83,7 +49,5 @@ public:
 		price = right.price;
 		return *this;
 	}
-
-
 };
 #endif // ICECREAMFLAVOR_H_INCLUDED
